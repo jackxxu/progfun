5850421ca6809ab81fa96ca8eabbfe2a48983ecb
Jack Xu <jackxxu@gmail.com>
2015-01-31 13:38:54 -0500

    tail recursive

--- hofun/src/main/scala/hofun/Sigma.scala
+++ hofun/src/main/scala/hofun/Sigma.scala
@@ -2,7 +2,13 @@ package hofun
 
 object SigmaObject {
   
-  def sigma(f: Int => Int, lower: Int, upper: Int): Int =
-    if(lower > upper ) 0
-    else f(lower) + sigma(f, lower + 1, upper)
+  def sigma(f: Int => Int, lower: Int, upper: Int): Int = {
+    def loop(sum: Int, lower: Int): Int = {
+      if(lower > upper) sum
+      else loop(sum + f(lower), lower + 1)          
+    }
+    
+    loop(0, lower)    
+  }
+    
 }
