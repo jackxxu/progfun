753b4451f3fce8008bda125e823219d40308a6e7
Jack Xu <jackxxu@gmail.com>
2015-01-31 16:04:51 -0500

    implement general function

--- hofun/src/main/scala/hofun/Sigma.scala
+++ hofun/src/main/scala/hofun/Sigma.scala
@@ -2,7 +2,7 @@ package hofun
 
 object SigmaObject {
   
-  def sigma(f: Int => Int, lower: Int, upper: Int): Int = {
+  def sigma(f: Int => Int, lower: Int, upper: Int) = {
     def loop(sum: Int, lower: Int): Int = {
       if(lower > upper) sum
       else loop(sum + f(lower), lower + 1)          
@@ -10,5 +10,29 @@ object SigmaObject {
     
     loop(0, lower)    
   }
+  
+  def product(f: Int => Int)(lower: Int, upper: Int): Int = {
+    if(lower > upper) 1
+    else f(lower)*product(f)(lower+1, upper)
+  }
+  
+  def factorial(number: Int): Int = product(x => x)(1, number)
+
+  def general(f: Int => Int, acc: Int)(reduce: (Int, Int) => Int)(lower: Int, upper: Int) : Int = {
+    def loop(total: Int, lower: Int): Int = {
+      if(lower > upper) total
+      else loop(reduce(total, f(lower)), lower + 1)          
+    }
     
+    loop(acc, lower)    
+  }
+  
+  def sigma2(f: Int => Int)(lower: Int, upper: Int) = {
+    general(f, 0)((x: Int, y: Int) => x + y)(lower, upper)
+  } 
+
+  def factorial2(number: Int): Int = {
+    general(x => x, 1)((x: Int, y: Int) => x*y)(1, number)
+  }
+  
 }
--- hofun/src/test/scala/hofun/TestSigma.scala
+++ hofun/src/test/scala/hofun/TestSigma.scala
@@ -20,5 +20,25 @@ class TestSigma extends FlatSpec {
    "return the total of the square value" in {
      assert(385 == SigmaObject.sigma((x: Int) => x*x, 1, 10))
    }
+   
+   "Product method when pass in a function and two integers" should 
+   "return the product of the function results of the numbers" in  {
+     assert(3628800 == SigmaObject.product(x => x)(1, 10))
+   }
 
+   "Factorial method when pass in a function and two integers" should 
+   "return the product of the function results of the numbers" in  {
+     assert(120 == SigmaObject.factorial(5))
+   }
+
+   "Sigma2 method when passed in a function that returns itself" should
+   "return the total of the value" in {
+     def self(x: Int) = x
+     assert(55 == SigmaObject.sigma2(self)(1, 10))
+   }
+
+   "Factorial2 method when pass in a function and two integers" should 
+   "return the product of the function results of the numbers" in  {
+     assert(120 == SigmaObject.factorial2(5))
+   }
 }
